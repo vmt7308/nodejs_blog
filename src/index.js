9cb804d7f1f36550e0e80f88861a6e6290f1d7dd
Minh Trung <vmt7308@gmail.com>
2025-02-13 20:17:10 +0700

    Refactor code: create routes, app/controllers folders - [MVC] Routes & Controllers

--- src/index.js
+++ src/index.js
@@ -5,12 +5,16 @@ const { engine } = require("express-handlebars");
 const app = express();
 const port = 3000;
 
+const route = require("./routes");
+
 app.use(express.static(path.join(__dirname, "public")));
 
 // Gửi dữ liệu (POST): XMLHttpRequest, fetch, axios, super agent, jquery ajax, ...
-app.use(express.urlencoded({
-  extended: true
-})); // Gửi (POST) ở dạng Middleware
+app.use(
+  express.urlencoded({
+    extended: true,
+  })
+); // Gửi (POST) ở dạng Middleware
 app.use(express.json()); // Gửi (POST) ở dạng JavaScript
 
 // HTTP logger
@@ -26,24 +30,8 @@ app.engine(
 app.set("view engine", "hbs");
 app.set("views", path.join(__dirname, "resources/views"));
 
-app.get("/", (req, res) => {
-  res.render("home");
-});
-
-app.get("/news", (req, res) => {
-  //console.log(req.query.q);
-  res.render("news");
-});
-
-app.get("/search", (req, res) => {
-  // console.log(req.query.q);
-  res.render("search");
-});
-
-app.post("/search", (req, res) => {
-  // console.log(req.body);
-  res.send("");
-});
+// Routes init
+route(app);
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
