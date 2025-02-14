490d46fa978a90dae59383418056b2d8c82defe0
Minh Trung <vmt7308@gmail.com>
2025-02-14 18:26:04 +0700

    [CRUD] Read from DB

--- src/app/controllers/SiteController.js
+++ src/app/controllers/SiteController.js
@@ -1,16 +1,17 @@
 const Course = require("../models/Course");
+const { multipleMongooseToObject } = require("../../util/mongoose");
 
 class SiteController {
   // [GET] /
-  index = async (req, res) => {
-    try {
-      const courses = await Course.find({});
-      res.json(courses);
-    } catch (error) {
-      res.status(400).json({ error: "ERROR!" });
-    }
-    // res.render('home');
-  };
+  index(req, res, next) {
+    Course.find({})
+      .then((courses) => {
+        res.render("home", {
+          courses: multipleMongooseToObject(courses),
+        });
+      })
+      .catch(next);
+  }
 
   // [GET] /search
   search(req, res) {
