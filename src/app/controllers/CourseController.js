9b91be84396637c3919c5d82d926b23a90e95ce2
Minh Trung <vmt7308@gmail.com>
2025-02-15 00:33:13 +0700

    [CRUD] Create new course

--- src/app/controllers/CourseController.js
+++ src/app/controllers/CourseController.js
@@ -5,10 +5,26 @@ class CourseController {
   // [GET] /courses/:slug
   show(req, res, next) {
     Course.findOne({ slug: req.params.slug })
-      .then(course => 
-        res.render('courses/show', { course: mongooseToObject(course) })
+      .then((course) =>
+        res.render("courses/show", { course: mongooseToObject(course) })
       )
       .catch(next);
+  }
+
+  // [GET] /courses/create
+  create(req, res, next) {
+    res.render("courses/create");
+  }
+
+  // [POST] /courses/store
+  store(req, res, next) {
+    const formData = req.body;
+    formData.image = `https://img.youtube.com/vi/${req.body.videoId}/sddefault.jpg`;
+    const course = new Course(formData);
+    course
+      .save()
+      .then(() => res.redirect('/'))
+      .catch((error) => {});
   }
 }
 
--- src/app/models/Course.js
+++ src/app/models/Course.js
@@ -1,12 +1,36 @@
 const mongoose = require("mongoose");
+const slugify = require("slugify");
+
 const Schema = mongoose.Schema;
 
-const Course = new Schema({
-  name: { type: String, maxLength: 255 },
-  description: { type: String, maxLength: 600 },
-  image: { type: String, maxLength: 255 },
-  createAt: { type: Date, default: Date.now },
-  updateAt: { type: Date, default: Date.now },
+const Course = new Schema(
+  {
+    name: { type: String, require: true },
+    description: { type: String },
+    image: { type: String },
+    videoId: { type: String, require: true },
+    level: { type: String },
+    slug: { type: String, slug: "name", unique: true },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+// Middleware tự động tạo slug nếu trùng
+Course.pre("save", async function (next) {
+  if (this.isModified("name") || this.isNew) {
+    let newSlug = slugify(this.name, { lower: true, strict: true });
+    let count = 0;
+
+    while (await mongoose.model("Course").exists({ slug: newSlug })) {
+      count++;
+      newSlug = `${slugify(this.name, { lower: true, strict: true })}-${count}`;
+    }
+
+    this.slug = newSlug;
+  }
+  next();
 });
 
 module.exports = mongoose.model("Course", Course);
