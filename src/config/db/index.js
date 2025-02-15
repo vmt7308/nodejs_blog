5126d1565cf9875f56893600c8edec96cbc8a8a2
Minh Trung <vmt7308@gmail.com>
2025-02-15 16:16:12 +0700

    [CRUD] Delete course

--- src/config/db/index.js
+++ src/config/db/index.js
@@ -2,10 +2,7 @@ const mongoose = require("mongoose");
 
 async function connect() {
   try {
-    await mongoose.connect("mongodb://localhost:27017/education_dev", {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
+    await mongoose.connect("mongodb://localhost:27017/education_dev");
     console.log("Connect successfully!");
   } catch (error) {
     console.log("Connect failure!");
